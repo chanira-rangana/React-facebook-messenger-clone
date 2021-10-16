4d538b66571b56635cb2a1e184b75b7eaf02dc49
Chanira <chanirapannala@gmail.com>
2021-10-16 16:28:08 +0530

    Create Send Message Button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import { Button } from "@mui/material";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["Hello", "How are you", "Shan"]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
 
   return (
     <div className="App">
       <h1>Facebook Messenger</h1>
-      <input type="text" onChange={(e) => setInput(e.target.value)} />
-      <button>Send Message</button>
-      <p>{input}</p>
+      <form>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button disabled={!input} variant="contained" type="submit" onClick={sendMessage}>
+          Send Message
+        </Button>
+      </form>
+      {messages.map((message) => (
+        <p key={message}>{message}</p>
+      ))}
     </div>
   );
 }
